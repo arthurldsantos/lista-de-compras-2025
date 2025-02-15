0a53212f024e8ab5828d65338f8113cfdfe52ae3
Arthur Santos <arthurldsantos@gmail.com>
2025-02-15 16:20:31 -0300

    criar arquivo para gerar a data completa

--- scripts/criarItemDaLista.js
+++ scripts/criarItemDaLista.js
@@ -1,3 +1,5 @@
+import gerarDiaDaSemana from "./gerarDiaDaSemana.js";
+
 const inputItem = document.getElementById('input-item');
 let contador = 0;
 
@@ -28,17 +30,8 @@ export function criarItemDaLista() {
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+    const dataCompleta = gerarDiaDaSemana();
 
-    const diaDaSemana = new Date().toLocaleDateString('pt-BR', {
-        weekday: 'long'
-    });
-    const data = new Date().toLocaleDateString('pt-BR');
-    const hora = new Date().toLocaleTimeString('pt-BR', {
-        hour: 'numeric',
-        minute: 'numeric'
-    })
-
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
     const itemData = document.createElement('p');
     itemData.innerText = dataCompleta;
     itemData.classList.add('texto-data');
@@ -47,4 +40,3 @@ export function criarItemDaLista() {
 
     return itemDaLista;
 }
-
